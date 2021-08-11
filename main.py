fd52aa355772b5f2ce91aa82f696229e12ab3825
gingersmith4 <88351672+gingersmith4@users.noreply.github.com>
2021-08-11 21:35:15 +0000

    curvy g

--- main.py
+++ main.py
@@ -1,25 +1,83 @@
-#draw my first initial (g) with the turtle!
 import turtle
+#draw my first initial (g) with the turtle!
+'''draw a capital g, starting with the line from the center'''
 
-def draw_picture(the_turtle):
-    ''' Draw a simple picture using a turtle '''
-    the_turtle.forward(100)
-    the_turtle.left(30)
-    the_turtle.forward(100)
-    the_turtle.left(120)
-    the_turtle.forward(100)
-    the_turtle.left(30)
-    the_turtle.forward(100)
-    the_turtle.left(120)    
+def draw_G(the_turtle):
+  #line of the G
+  the_turtle.forward(50)
+  the_turtle.right(90)
+  the_turtle.forward(40)
+  #right half of bottom curve
+  for x in range(3):
+    the_turtle.right(30)
+    the_turtle.forward(20)
+  #left half of bottom curve
+  for x in range(3):
+    the_turtle.forward(20)
+    the_turtle.right(30)
+  the_turtle.forward(90)
+  for x in range(3):
+    the_turtle.right(30)
+    the_turtle.forward(20)
+  for x in range(3):
+    the_turtle.forward(20)
+    the_turtle.right(30)
 
-my_turtle = turtle.Turtle()  # Create a new Turtle object
-draw_picture(my_turtle)      # make the new Turtle draw the shape
+my_turtle = turtle.Turtle()
 
-#creates two more turtles
-turtle1 = turtle.Turtle()
-turtle2 = turtle.Turtle()
-turtle1.setpos(-50, -50)
-turtle2.setpos(200, 100)
-turtle1.forward(100)
-turtle2.left(90)
-turtle2.forward(100)
+#draw_G(my_turtle)
+
+second_turtle = turtle.Turtle()
+second_turtle.penup()
+second_turtle.setpos(150,0)
+second_turtle.pendown()
+#draw_G(second_turtle)
+
+#turtle1.setpos(-50, -50)
+x_coordinate = 0
+y_coordinate = 0
+
+def draw_G(the_turtle, size):
+    '''Turtle2 draws the initial "G" '''
+    
+    the_turtle.pensize(size)
+
+    the_turtle.penup()
+   
+    the_turtle.goto(x_coordinate + 200, y_coordinate)
+
+    the_turtle.right(180)
+
+    the_turtle.pendown()
+
+    the_turtle.circle(100,270)
+
+    the_turtle.left(90)
+
+    the_turtle.forward(50)
+
+    the_turtle.backward(60)
+
+    the_turtle.left(90)
+
+    the_turtle.forward(100)
+
+    the_turtle.right(180)
+
+    the_turtle.penup()
+
+    the_turtle.forward(200)
+
+    the_turtle.left(90)
+
+    the_turtle.forward(110)
+
+    the_turtle.left(90)
+
+    the_turtle.pendown()
+
+    the_turtle.forward(20)
+
+turtleG = turtle.Turtle() # Create a second new Turtle object
+
+draw_G(turtleG, 5)      # make the new turtle draw the letter "G"
